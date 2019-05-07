899339b0e52a875e0692d3c5d2727f0de86f6ef7
Erica P <erica.parra@rocketmail.com>
2019-05-06 20:18:50 -0700

    Inserted mock data

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,15 @@
 --     stock_quantity INT NOT NULL,
 --     PRIMARY KEY (item_id)
 -- );
+
+-- INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+-- VALUES -- (1, "Tent" , "Camping", 80.50, 10),
+--  (2, "Basketball", "Sports", 20, 5),
+--  (3, "Sleeping Bag", "Camping", 25, 15), 
+--  (4, "Skateboard", "Sports", 50, 5), 
+--  (5 , "Ring" , "Jewelry", 150, 2), 
+--  (6, "Necklace", "Jewelry", 75, 8), 
+--  (7, "Fishing Pole", "Sports", 60, 9), 
+--  (8, "Crayons", "Arts" , 5, 50),
+--  (9 , "Stapler", "Office", 10, 20),
+--  (10, "Notebook", "Office", 8, 100);
